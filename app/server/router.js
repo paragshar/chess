5c4a8bc23598228d96576d2a08f38e3973c2d30d
Rakesh Makam <rakesh@mantralabsglobal.com>
2013-08-09 15:43:37 +0530

    adding log

--- app/server/router.js
+++ app/server/router.js
@@ -262,8 +262,8 @@ module.exports = function(app) {
 	
 	app.get('/playerslist', function(req, res) {
 	    AM.getAllRecords( function(e, accounts){
-	    	console.log(accounts);
-			res.render('playerslist', { title : 'Players List', accts : accounts });
+	    	console.log(req.session.user);
+			res.render('playerslist', { title : 'Players List', accts : accounts , user: req.session.user});
 		})
 	});
 
